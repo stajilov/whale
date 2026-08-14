8098d990a5cffce1f031aca9577a822175a2fd11
stajilov <stazhilov@gmail.com>
2026-08-14 14:02:10 +0300

    feature: typer for commands added

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from whale-harness!")
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
-    main()
+    app()
